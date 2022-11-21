0cf3f2aba13a0a0ddbc72f11d60f5d6691a9996b
Harish Kunta <33089746+harishtanu007@users.noreply.github.com>
2022-11-21 02:23:32 -0800

    Login Activity clean up

--- app/src/main/java/com/harish/tinder/material_ui/LoginActivity.java
+++ app/src/main/java/com/harish/tinder/material_ui/LoginActivity.java
@@ -1,39 +1,40 @@
 package com.harish.tinder.material_ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.harish.tinder.ForgotPasswordActivity;
 import com.harish.tinder.R;
+
 import android.content.Intent;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.LinearLayout;
+
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.harish.tinder.utils.StringResourceHelper;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button sign_in, sign_up;
-    private TextView forgotPassword;
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
     private TextInputEditText editTextEmail, editTextPassword;
-    private TextInputLayout emailLayout,passwordLayout;
+    private TextInputLayout emailLayout, passwordLayout;
     private LinearLayout rootLayout;
     private String email, password;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    private Context context;
 
-    private static final String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,32 +42,34 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = firebaseAuth -> {
             final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            if (user !=null){
+            if (user != null) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
-                return;
             }
         };
-        rootLayout = (LinearLayout) findViewById(R.id.root_layout);
-        sign_in = (Button)findViewById(R.id.sign_in_button);
-        sign_up = (Button)findViewById(R.id.sign_up_button);
-        editTextEmail = (TextInputEditText)findViewById(R.id.email_field);
-        editTextPassword = (TextInputEditText)findViewById(R.id.password_field);
-        emailLayout = (TextInputLayout)findViewById(R.id.username_field_input_layout);
-        passwordLayout = (TextInputLayout)findViewById(R.id.password_field_input_layout);
-        forgotPassword = (TextView)findViewById(R.id.forgot_password);
+        rootLayout = findViewById(R.id.root_layout);
+        Button sign_in = findViewById(R.id.sign_in_button);
+        Button sign_up = findViewById(R.id.sign_up_button);
+        editTextEmail = findViewById(R.id.email_field);
+        editTextPassword = findViewById(R.id.password_field);
+        emailLayout = findViewById(R.id.username_field_input_layout);
+        passwordLayout = findViewById(R.id.password_field_input_layout);
+        TextView forgotPassword = findViewById(R.id.forgot_password);
+        context = getApplicationContext();
         editTextEmail.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length() == 0)
-                    emailLayout.setError("Please enter username");
+                if (s.length() == 0)
+                    emailLayout.setError(StringResourceHelper.getString(context, R.string.enter_user_name));
                 else
                     emailLayout.setError(null);
             }
@@ -75,66 +78,37 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length() == 0)
-                    passwordLayout.setError("Please enter password");
+                if (s.length() == 0)
+                    passwordLayout.setError(StringResourceHelper.getString(context, R.string.enter_password));
                 else
                     passwordLayout.setError(null);
             }
         });
 
-        //TODO : forgot password link
+        forgotPassword.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class)));
 
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(LoginActivity.this, "you can reset your password now", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
-            }
-        });
-
-        sign_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email = editTextEmail.getText().toString();
-                password = editTextPassword.getText().toString();
-                if(email.trim().length() > 0 && password.trim().length() > 0){
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Snackbar.make(rootLayout, "Incorrect username or password", Snackbar.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-//                    String uName = null, pWord = null;
-//                    if (mSharedPreferences.contains("username")){
-//                        uName = mSharedPreferences.getString("username","");
-//                    }
-//                    if (mSharedPreferences.contains("password")){
-//                        pWord = mSharedPreferences.getString("password","");
-//                    }
-//
-//
-//                    if(username.equals(uName) && password.equals(pWord)){
-//                        Toast.makeText(getApplicationContext(),"Login Successful", Toast.LENGTH_LONG).show();
-//                        Intent welcome = new Intent(getApplicationContext(), RegistrationActivity.class);
-//                        startActivity(welcome);
-//                    }
-//                    else{
-//                        Toast.makeText(getApplicationContext(), "Username/Password is incorrect"+uName+pWord, Toast.LENGTH_LONG).show();
-//                    }
-                } else if (email.length() <= 0) {
-                Snackbar.make(rootLayout, "Enter username", Snackbar.LENGTH_LONG).show();
+        sign_in.setOnClickListener(v -> {
+            email = Objects.requireNonNull(editTextEmail.getText()).toString();
+            password = Objects.requireNonNull(editTextPassword.getText()).toString();
+            if (email.trim().length() > 0 && password.trim().length() > 0) {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
+                    if (!task.isSuccessful()) {
+                        Snackbar.make(rootLayout, StringResourceHelper.getString(context, R.string.incorrect_username_or_password), Snackbar.LENGTH_LONG).show();
+                    }
+                });
+            } else if (email.length() <= 0) {
+                Snackbar.make(rootLayout, StringResourceHelper.getString(context, R.string.enter_user_name), Snackbar.LENGTH_LONG).show();
             } else if (password.length() <= 0) {
-                Snackbar.make(rootLayout, "Enter password", Snackbar.LENGTH_LONG).show();
-            }else{
-                    Snackbar.make(rootLayout, "Please enter username and password", Snackbar.LENGTH_LONG).show();
-                }
+                Snackbar.make(rootLayout, StringResourceHelper.getString(context, R.string.enter_password), Snackbar.LENGTH_LONG).show();
+            } else {
+                Snackbar.make(rootLayout, StringResourceHelper.getString(context, R.string.enter_username_and_password), Snackbar.LENGTH_LONG).show();
             }
         });
 
@@ -144,6 +118,7 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         });
     }
+
     @Override
     protected void onStart() {
         super.onStart();
